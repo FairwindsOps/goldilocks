6d5c5287d15c496942323ac9046058ddf95a9e1d
Robert Brennan <bobby.brennan@gmail.com>
2019-10-23 21:33:23 +0000

    add yq as e2e dependency

--- e2e/test.sh
+++ e2e/test.sh
@@ -18,6 +18,8 @@ echo "**************************"
 printf "\n\n"
 
 apk --update add openssl
+wget -O /usr/local/bin/yq "https://github.com/mikefarah/yq/releases/download/2.4.0/yq_linux_amd64"
+chmod +x /usr/local/bin/yq
 
 printf "\n\n"
 echo "****************************"
