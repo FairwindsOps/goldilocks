2ed086b3e72b749d4ebaf48d09db99b2bb28e9db
Harrison Katz <hkatz@squarespace.com>
2019-11-26 19:15:18 -0500

    Split templates and dashboard into files ; Make template util functions more flexible for the router and for new webpages

--- pkg/web/dashboard.go
+++ pkg/web/dashboard.go
@@ -15,64 +15,15 @@
 package web
 
 import (
-	"bytes"
-	"encoding/json"
-	"html/template"
 	"net/http"
 
-	packr "github.com/gobuffalo/packr/v2"
 	"github.com/gorilla/mux"
 	"k8s.io/klog"
 
 	"github.com/fairwindsops/goldilocks/pkg/summary"
-	"github.com/fairwindsops/goldilocks/pkg/web/helpers"
 )
 
-// templates
-const (
-	// MainTemplateName is the main template
-	MainTemplateName = "main.gohtml"
-	// HeadTemplateName contains styles and meta info
-	HeadTemplateName = "head.gohtml"
-	// NavbarTemplateName contains the navbar
-	NavbarTemplateName = "navbar.gohtml"
-	// PreambleTemplateName contains an empty preamble that can be overridden
-	PreambleTemplateName = "preamble.gohtml"
-	// DashboardTemplateName contains the content of the dashboard
-	DashboardTemplateName = "dashboard.gohtml"
-	// NamespaceTemplateName contains the content for a namespace
-	NamespaceTemplateName = "namespace.gohtml"
-	// ContainerTemplateName contains the content for a container
-	ContainerTemplateName = "container.gohtml"
-	// FooterTemplateName contains the footer
-	FooterTemplateName = "footer.gohtml"
-	// CheckDetailsTemplateName is a page for rendering details about a given check
-	CheckDetailsTemplateName = "check-details.gohtml"
-)
-
-var templateBox = (*packr.Box)(nil)
-
-// templateData is passed to the dashboard HTML template
-type templateData struct {
-	// BasePath is the base URL that goldilocks is being served on, used in templates for html base
-	BasePath string
-
-	// Summary is the summary information to display on the rendered dashboard
-	Summary summary.Summary
-
-	// JSON is the json version of the summary data provided to the html window object for debugging and user interaction
-	JSON template.JS
-}
-
-// getTemplateBox returns a binary-friendly set of templates for rendering the dash
-func getTemplateBox() *packr.Box {
-	if templateBox == (*packr.Box)(nil) {
-		templateBox = packr.New("Templates", "templates")
-	}
-	return templateBox
-}
-
-// DashboardAllNamespaces replies with the rendered dashboard (on the basePath) for the summarizer
+// Dashboard replies with the rendered dashboard (on the basePath) for the summarizer
 func Dashboard(opts Options) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
@@ -95,79 +46,19 @@ func Dashboard(opts Options) http.Handler {
 			http.Error(w, "Error running summary.", http.StatusInternalServerError)
 			return
 		}
-		jsonData, err := json.Marshal(vpaData)
-		if err != nil {
-			http.Error(w, "Error serializing summary vpaData", http.StatusInternalServerError)
-			return
-		}
 
-		data := templateData{
-			BasePath: opts.basePath,
-			Summary:  vpaData,
-			JSON:     template.JS(jsonData),
-		}
-		tmpl, err := getTemplate("main")
+		tmpl, err := getTemplate("dashboard",
+			"check-details",
+			"container",
+			"namespace",
+			"dashboard",
+		)
 		if err != nil {
 			klog.Errorf("Error getting template data %v", err)
 			http.Error(w, "Error getting template data", http.StatusInternalServerError)
 			return
 		}
 
-		writeTemplate(tmpl, &data, w)
+		writeTemplate(tmpl, opts, &vpaData, w)
 	})
-}
-
-// getTemplate puts together the dashboard template. Individual pieces can be overridden before rendering.
-func getTemplate(name string) (*template.Template, error) {
-	tmpl := template.New(name).Funcs(template.FuncMap{
-		"printResource":  helpers.PrintResource,
-		"getStatus":      helpers.GetStatus,
-		"getStatusRange": helpers.GetStatusRange,
-		"resourceName":   helpers.ResourceName,
-		"getUUID":        helpers.GetUUID,
-	})
-
-	templateFileNames := []string{
-		DashboardTemplateName,
-		NamespaceTemplateName,
-		ContainerTemplateName,
-		HeadTemplateName,
-		NavbarTemplateName,
-		PreambleTemplateName,
-		FooterTemplateName,
-		MainTemplateName,
-	}
-
-	return parseTemplateFiles(tmpl, templateFileNames)
-}
-
-func parseTemplateFiles(tmpl *template.Template, templateFileNames []string) (*template.Template, error) {
-	templateBox := getTemplateBox()
-	for _, fname := range templateFileNames {
-		templateFile, err := templateBox.Find(fname)
-		if err != nil {
-			return nil, err
-		}
-
-		tmpl, err = tmpl.Parse(string(templateFile))
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return tmpl, nil
-}
-
-func writeTemplate(tmpl *template.Template, data *templateData, w http.ResponseWriter) {
-	buf := &bytes.Buffer{}
-	err := tmpl.Execute(buf, data)
-	if err != nil {
-		klog.Errorf("Error executing template: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	_, err = buf.WriteTo(w)
-	if err != nil {
-		klog.Errorf("Error writing template: %v", err)
-	}
 }
