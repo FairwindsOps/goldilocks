8fb4534e9e8b81e527f642376772bfa9d40202fb
Harrison Katz <hkatz@squarespace.com>
2019-11-27 15:24:11 -0500

    Add namespace list search bar/filter ; Add some friendly js improvements

---
+++ pkg/dashboard/assets/js/namespace_list.js
@@ -0,0 +1,37 @@
+$(function () {
+    var $namespaceCards = $("#namespaceCards .card")
+    var $searchBox = $("#search")
+
+    // filters namespace cards containing the search text
+    function filterNamespaceCards() {
+        var searchText = $searchBox.val()
+        $namespaceCards.filter(function() {
+            var cardNamespace = $("strong", $(this)).text()
+            var cardContainsSearchText = cardNamespace.toLowerCase().indexOf(searchText.toLowerCase()) > -1
+            if(cardContainsSearchText) {
+                $(this).show()
+            } else {
+                $(this).hide()
+            }
+        })
+    }
+
+    // clicks on the "view" button for the first visible namespace card
+    function viewDashboardForFirstNamespace() {
+        $namespaceCards.filter("div:not([style='display: none;'])").first().find("a[data-name='view'")[0].click()
+    }
+
+    // filter namespace cards on every key typed
+    $searchBox.on("keyup", filterNamespaceCards)
+
+    // visit the first filtered namespace card's view/dashboard page by pressing enter
+    $searchBox.on("keypress", function(e) {
+        var key = e.which;
+        if(key == 13) { // enter
+            viewDashboardForFirstNamespace()
+        }
+    })
+
+    // start with a filter incase the browser caches input data
+    filterNamespaceCards()
+})
