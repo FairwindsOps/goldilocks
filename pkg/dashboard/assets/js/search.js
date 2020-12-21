5719bf9853c8a579a68d1569ade3f3f83f99be09
Daine <littlelark@users.noreply.github.com>
2020-12-20 17:04:28 -0700

    update search.js to handle missing search form or malformed data attributes

--- pkg/dashboard/assets/js/search.js
+++ pkg/dashboard/assets/js/search.js
@@ -3,11 +3,11 @@
     const containerId = "js-search-container";
 
     const form = document.getElementById(formId);
-    const searchInput = form.querySelector("input[type='search']");
+    const searchInput = form?.querySelector("input[type='search']");
 
     const container = document.getElementById(containerId);
-    const potentialResults = container.querySelectorAll("[data-search]");
-    const numPotentialResults = potentialResults.length;
+    const potentialResults = container?.querySelectorAll("[data-search]");
+    const numPotentialResults = potentialResults?.length;
 
     function showSearchResult(result) {
         result.style.removeProperty("display");
@@ -44,10 +44,17 @@
         }
     }
 
-    searchInput.addEventListener("input", updateResults);
-    
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        updateResults();
-    })
+    if (form && container) {
+        if (numPotentialResults === 0) {
+            form.style.display = "none";
+            console.error("No filterable entries found, removed filter form");
+        } else {
+            searchInput.addEventListener("input", updateResults);
+            
+            form.addEventListener("submit", function(event) {
+                event.preventDefault();
+                updateResults();
+            })
+        }
+    }
 })();
