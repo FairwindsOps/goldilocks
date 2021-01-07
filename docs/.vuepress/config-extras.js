e118943aebc06d2cb629f34faa14e1634a0e9ced
Andrew Suderman <andrew@sudermanjr.com>
2021-01-07 10:44:56 -0700

    Adding all docs stuff

---
+++ docs/.vuepress/config-extras.js
@@ -0,0 +1,10 @@
+// To see all options:
+// https://vuepress.vuejs.org/config/
+// https://vuepress.vuejs.org/theme/default-theme-config.html
+module.exports = {
+  title: "goldilocks Documentation",
+  description: "Documentation for Fairwinds' goldilocks",
+  themeConfig: {
+    docsRepo: "FairwindsOps/goldilocks",
+  },
+}
