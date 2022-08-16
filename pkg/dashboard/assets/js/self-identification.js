0c975318def2b4968846a1f56498c60c77857e7d
hieptl <hieptl.developer@gmail.com>
2022-08-17 00:19:29 +0700

    remove toggleSubmitBtn logic and updated css

--- pkg/dashboard/assets/js/self-identification.js
+++ pkg/dashboard/assets/js/self-identification.js
@@ -35,40 +35,18 @@
   }
 
   emailInput.addEventListener("input", function (evt) {
-    const checked = emailCheckbox.checked;
     toggleLabelContent(this.value);
-    toggleSubmitBtn(checked);
-  });
-
-  emailCheckbox.addEventListener("change", function () {
-    toggleSubmitBtn(this.checked);
   });
 
   function toggleLabelContent(inputEmail) {
     emailLabelContent.style.display = inputEmail ? "none" : "inline-block";
   }
 
-  function toggleSubmitBtn(checked) {
-    if (isInputInfoValid(checked)) {
-      submitBtn.disabled = false;
-      submitBtn.classList.add("email-box__submit-btn--active");
-    } else {
-      submitBtn.disabled = true;
-      submitBtn.classList.remove("email-box__submit-btn--active");
-    }
-  }
-
   submitBtn.addEventListener("click", function () {
-    const email = emailInput.value;
-    const checked = emailCheckbox.checked;
-    if (isInputInfoValid(email, checked)) {
+    if (emailCheckbox.checked && emailInput.validity.valid) {
       // TODO: write logic to call the API to get the access token.
       window.location.reload();
       localStorage.setItem("emailEntered", true);
     }
   });
-
-  function isInputInfoValid(checked) {
-    return checked && emailInput.validity.valid;
-  }
 })();
