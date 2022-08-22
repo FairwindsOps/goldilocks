1bfb06c51c66ee4941adf1559c78b9f8dddd2ffb
Hiep Le <69354317+hieptl@users.noreply.github.com>
2022-08-22 20:46:06 +0700

    Develop the API Token Section (#507)
    * developed the UI for the email-box
    
    * added validation and query params
    
    * removed CSS comments, unusded class attribute, and changed the scripts order
    
    * updated css for the email input
    
    * removed the validator library
    
    * changed div to aside and used the form tag
    
    * used accent-color, added label
    
    * removed outline:none
    
    * updated the outline css for the email input
    
    * changed h2 to p
    
    * make the privacy policy text as a link
    
    * updated the background color for the submit button
    
    * updated the text color for the email label
    
    * updated the accent-color for the checkbox
    
    * updated the text color the actived button and its outline
    
    * moved the link outside the label
    
    * remove toggleSubmitBtn logic and updated css
    
    * moved the link outside label
    
    * added required for the checkbox
    
    * udpdated the text color for the privacy link and added the underline
    
    * integrated with the api to get the API token
    
    * chnaged insightshost to insights-host
    
    * developed the api token section
    
    * changed the default value for insights-host
    
    * updated the text, added the disable cost settings button
    
    * add css to disable the buttons when the forms are invalid

---
+++ pkg/dashboard/assets/js/api-token.js
@@ -0,0 +1,65 @@
+(function () {
+  const apiTokenBoxId = "api-token-box";
+  const disableCostSettingsBtnId = "api-token__disable-cost-settings";
+  const apiTokenlLabelContentId = "api-token-box__api-token-label-content";
+  const apiTokenInputId = "api-token-box__api-token-input";
+  const submitBtnId = "api-token-box__submit-btn";
+
+  const apiTokenBox = document.getElementById(apiTokenBoxId);
+  const disableCostSettingsBtn = document.getElementById(
+    disableCostSettingsBtnId
+  );
+  const apiTokenLabelContent = document.getElementById(apiTokenlLabelContentId);
+  const apiTokenInput = document.getElementById(apiTokenInputId);
+  const submitBtn = document.getElementById(submitBtnId);
+
+  const apiKey = localStorage.getItem("apiKey");
+  const isEmailEntered = localStorage.getItem("emailEntered");
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  initQueryParams();
+  initUIState();
+
+  function initQueryParams() {
+    if (apiKey && !urlParams.has("apiKey")) {
+      setQueryParam("apiKey", apiKey);
+    }
+  }
+
+  function setQueryParam(key, value) {
+    urlParams.set(key, value);
+    window.location.search = urlParams;
+  }
+
+  function initUIState() {
+    if (apiKey || !isEmailEntered) {
+      apiTokenBox.style.display = "none";
+    }
+  }
+
+  apiTokenInput.addEventListener("input", function () {
+    toggleLabelContent(this.value);
+  });
+
+  function toggleLabelContent(inputApiToken) {
+    apiTokenLabelContent.style.display = inputApiToken
+      ? "none"
+      : "inline-block";
+  }
+
+  submitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (apiTokenInput.validity.valid) {
+      window.location.reload();
+      localStorage.setItem("apiKey", apiTokenInput.value);
+    }
+  });
+
+  disableCostSettingsBtn.addEventListener("click", function () {
+    localStorage.removeItem("emailEntered");
+    localStorage.removeItem("apiKey");
+
+    window.location.href = window.location.href.split("?")[0];
+  });
+})();
---
+++ pkg/dashboard/assets/js/email.js
@@ -0,0 +1,70 @@
+(function () {
+  const emailBoxId = "email-box";
+  const emailLabelContentId = "email-box__email-label-content";
+  const emailInputId = "email-box__email-input";
+  const emailCheckboxId = "email-box__checkbox";
+  const submitBtnId = "email-box__submit-btn";
+
+  const emailBox = document.getElementById(emailBoxId);
+  const emailLabelContent = document.getElementById(emailLabelContentId);
+  const emailInput = document.getElementById(emailInputId);
+  const emailCheckbox = document.getElementById(emailCheckboxId);
+  const submitBtn = document.getElementById(submitBtnId);
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  initQueryParams();
+  initUIState();
+
+  function initQueryParams() {
+    const isEmailEntered = localStorage.getItem("emailEntered");
+    if (isEmailEntered && !urlParams.has("emailEntered")) {
+      setQueryParam("emailEntered", "true");
+    }
+  }
+
+  function setQueryParam(key, value) {
+    urlParams.set(key, value);
+    window.location.search = urlParams;
+  }
+
+  function initUIState() {
+    if (urlParams.get("emailEntered")) {
+      emailBox.style.display = "none";
+    }
+  }
+
+  emailInput.addEventListener("input", function (evt) {
+    toggleLabelContent(this.value);
+  });
+
+  function toggleLabelContent(inputEmail) {
+    emailLabelContent.style.display = inputEmail ? "none" : "inline-block";
+  }
+
+  submitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (emailCheckbox.checked && emailInput.validity.valid) {
+      fetch(`${window.INSIGHTS_HOST}/v0/oss/users`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: emailInput.value,
+          project: "goldilocks",
+        }),
+      }).then((response) => {
+        if (response) {
+          response.json().then((data) => {
+            if (data?.token) {
+              window.location.reload();
+              localStorage.setItem("emailEntered", true);
+            }
+          });
+        }
+      });
+    }
+  });
+})();
