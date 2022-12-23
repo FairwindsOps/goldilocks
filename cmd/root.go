9cf6d17539e3b59cb3b14cd6cccff373c26c7c2f
Stevie Caldwell <4719798+transient1@users.noreply.github.com>
2022-12-23 11:01:25 -0500

    Add PLG links to root.go

--- cmd/root.go
+++ cmd/root.go
@@ -26,6 +26,7 @@ import (
 
 var kubeconfig string
 var nsName string
+var exitCode int
 
 var (
 	version string
@@ -66,7 +67,10 @@ var rootCmd = &cobra.Command{
 		if err != nil {
 			klog.Error(err)
 		}
-		os.Exit(1)
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		os.Stderr.WriteString("\n\nWant more? Upgrade to the free tier of Fairwinds Insights! https://fairwinds.com/insights-signup/goldilocks 🚀 \n")
+		os.Exit(exitCode)
 	},
 }
 
