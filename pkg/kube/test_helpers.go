e4555a1b7b3152cef9521a27089b2556278839b4
jdesouza <james@fairwinds.com>
2026-02-23 11:12:29 -0300

    Revert "Go fix"
    This reverts commit a32a9afc9157650d6465c3ee4edc3fc10d9fc946.

--- pkg/kube/test_helpers.go
+++ pkg/kube/test_helpers.go
@@ -15,8 +15,7 @@ import (
 // GetMockClient returns a fake client instance for mocking
 func GetMockClient() *ClientInstance {
 	kc := ClientInstance{
-		// SA1019: NewSimpleClientset is deprecated in favor of NewClientset (requires applyconfig codegen); keep for test compatibility.
-		Client: fake.NewSimpleClientset(), //nolint:staticcheck // SA1019
+		Client: fake.NewSimpleClientset(),
 	}
 	SetInstance(kc)
 	return &kc
